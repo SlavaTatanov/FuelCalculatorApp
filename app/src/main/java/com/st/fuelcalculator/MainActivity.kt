bac0d1cc012aac09b73f8511dd1336db94113b34
SlavaTatanov <slavatatanov@gmail.com>
2023-01-20 22:34:49 +0500

    refactoring, migration to ViewBinding

--- app/src/main/java/com/st/fuelcalculator/MainActivity.kt
+++ app/src/main/java/com/st/fuelcalculator/MainActivity.kt
@@ -3,49 +3,28 @@ package com.st.fuelcalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import com.st.fuelcalculator.databinding.ActivityMainBinding
 import com.st.fuelcalculator.fuel.FuelCalculator
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var btnCalc: Button
-    private lateinit var btnSave: Button
-    private lateinit var btnPeopleMinus: Button
-    private lateinit var btnPeoplePlus: Button
-    private lateinit var editTextKm: EditText
-    private lateinit var editTextCons: EditText
-    private lateinit var editTextPrice: EditText
-    private lateinit var textViewPeople: TextView
-    private lateinit var textViewResult: TextView
+    private lateinit var binding :ActivityMainBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        /*
-        Блок назначающий глобальныи переменным их значения из интерфейса
-         */
-        btnCalc= findViewById(R.id.btn_calc)
-        btnSave = findViewById(R.id.btn_save)
-        btnPeopleMinus = findViewById(R.id.btnPeopleMinus)
-        btnPeoplePlus = findViewById(R.id.btnPeoplePlus)
-        editTextKm = findViewById(R.id.editTextKm)
-        editTextCons = findViewById(R.id.editTextCons)
-        editTextPrice = findViewById(R.id.editTextPrice)
-        textViewPeople = findViewById(R.id.peopleTextView)
-        textViewResult = findViewById(R.id.textViewResult)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         /*
         Блок устанавливает слушателей
          */
-        btnCalc.setOnClickListener { result(btnSave) }
-        btnPeopleMinus.setOnClickListener { peopleTextViewChange(-1) }
-        btnPeoplePlus.setOnClickListener { peopleTextViewChange(1) }
-        btnSave.setOnClickListener { secret() }
+        binding.btnCalc.setOnClickListener { result() }
+        binding.btnPeopleMinus.setOnClickListener { peopleTextViewChange(-1) }
+        binding.btnPeoplePlus.setOnClickListener { peopleTextViewChange(1) }
+        binding.btnSave.setOnClickListener { secret() }
 
         }
 
@@ -56,18 +35,18 @@ class MainActivity : AppCompatActivity() {
      * Результат записывается в TextView. Выводит кнопку сохранить,
      * предлагая юзеру сохранить расчет.
      */
-    private fun result (btnSave: Button) {
-        val km = editTextKm.text.toString().toDouble()
-        val cons = editTextCons.text.toString().toDouble()
-        val price = editTextPrice.text.toString().toDouble()
-        val people = textViewPeople.text.toString().toInt()
+    private fun result () {
+        val km = binding.editTextKm.text.toString().toDouble()
+        val cons = binding.editTextCons.text.toString().toDouble()
+        val price = binding.editTextPrice.text.toString().toDouble()
+        val people = binding.textViewPeople.text.toString().toInt()
         val res = FuelCalculator(km, cons, price, people).calculatingStr()
-        textViewResult.text = res
-        btnSave.visibility = View.VISIBLE
+        binding.textViewResult.text = res
+        binding.btnSave.visibility = View.VISIBLE
     }
 
     private fun secret () {
-        btnCalc.visibility = View.INVISIBLE
+        binding.btnCalc.visibility = View.INVISIBLE
     }
 
     /**
@@ -75,11 +54,11 @@ class MainActivity : AppCompatActivity() {
      * шаг - 1
      */
     private fun peopleTextViewChange (people: Int) {
-        var res = textViewPeople.text.toString().toInt()
+        var res = binding.textViewPeople.text.toString().toInt()
         if (peopleChecking(people)) {
             res += people
             if (res >= 1) {
-                textViewPeople.text = res.toString()
+                binding.textViewPeople.text = res.toString()
             }
         }
     }
